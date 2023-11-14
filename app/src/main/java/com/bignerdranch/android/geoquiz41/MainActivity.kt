cde44ff63bddd60d56483ec33bd1a94c965c0707
ikstami <k.mus00@yandex.ru>
2023-11-14 20:23:36 +0700

    Первый запуск эмулятора. На взлёт

--- app/src/main/java/com/bignerdranch/android/geoquiz41/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz41/MainActivity.kt
@@ -2,7 +2,9 @@ package com.bignerdranch.android.geoquiz41
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -14,5 +16,18 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.true_button)
         falseButton =
             findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
     }
